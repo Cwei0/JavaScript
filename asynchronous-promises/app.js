26faf084347eaf0093afaf3dba3399dcc6761639
Cwei0 <bottgabriel@icloud.com>
2022-12-07 04:36:15 +0100

    v4

--- asynchronous-promises/app.js
+++ asynchronous-promises/app.js
@@ -15,15 +15,33 @@ const promise1 = new Promise((resolve, reject) => {
         isReady ? resolve("soup is ready") : reject("soup is not ready");
     }, 3000);
 });
+const orderTime = async () => {
 
-console.log(
-    promise1
-        .then(success => console.log({ success }))
-        .catch(error => console.log({ error }))
-)
+    try{
+        const soup = await promise1
+        console.log(soup)
+    } catch (error) {
+        console.log(error)
+    }
+};
+orderTime();
 
-console.log('fetch:', 
-fetch("https://dog.ceo/api/breeds/image/random")
-.then(response => response.json())
-.then(data => console.log(data))
-)
+// console.log(
+//     promise1
+//         .then(success => console.log({ success }))
+//         .catch(error => console.log({ error }))
+// )
+
+// console.log('fetch:', 
+// fetch("https://dog.ceo/api/breeds/image/random")
+// .then(response => response.json())
+// .then(data => console.log(data))
+// )
+
+const getDog = async () => {
+    const response =  await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await response.json();
+    console.log(data);
+};
+
+getDog();
