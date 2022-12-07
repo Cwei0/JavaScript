f24f6d96a532d2eaf18603b35f9bc03d90256f91
Cwei0 <bottgabriel@icloud.com>
2022-12-07 05:00:37 +0100

    v5

--- asynchronous-promises/app.js
+++ asynchronous-promises/app.js
@@ -14,17 +14,29 @@ const promise1 = new Promise((resolve, reject) => {
         isReady = [true, false][Math.floor(Math.random() * 2)];
         isReady ? resolve("soup is ready") : reject("soup is not ready");
     }, 3000);
-});
+}); 
+
+// RESOLVED - if the waiter brings you soup, you tip the waiter
+// REJECT - leave a bad review, no tip
 const orderTime = async () => {
+    const review = {rating: 0, tip: 0, pay: 0}
 
     try{
         const soup = await promise1
+        review.rating = 5
+        review.tip = .2
+        review.pay = 10
         console.log(soup)
+        return review
     } catch (error) {
         console.log(error)
+        review.rating = 1
+        review.tip = 0
+        review.pay = 0
+        return review
     }
 };
-orderTime();
+orderTime().then(value => console.log(value))
 
 // console.log(
 //     promise1
@@ -38,10 +50,10 @@ orderTime();
 // .then(data => console.log(data))
 // )
 
-const getDog = async () => {
-    const response =  await fetch("https://dog.ceo/api/breeds/image/random");
-    const data = await response.json();
-    console.log(data);
-};
+// const getDog = async () => {
+//     const response =  await fetch("https://dog.ceo/api/breeds/image/random");
+//     const data = await response.json();
+//     console.log(data);
+// };
 
-getDog();
+// getDog();
